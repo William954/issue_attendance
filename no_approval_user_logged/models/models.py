9358141916bf24e726598114898d9d15b1ae57e8
William <williamcolin954@gmail.com>
2019-05-08 18:47:33 -0500

    Modificacion del res.users

--- no_approval_user_logged/models/models.py
+++ no_approval_user_logged/models/models.py
@@ -5,12 +5,12 @@ from odoo import models, fields, api
 class HrLeave(models.Model):
     _inherit = 'hr.leave'
 
-    user_logged_id = fields.Many2one('res.user', string='Solicitante', default=lambda self: self.env.user, track_visibility=True)
-    logged_id = fields.Boolean(string='No approval user logged',compute="_active_id")
+    user_logged_id = fields.Many2one('res.users', string='Solicitante', default=lambda self: self.env.user, track_visibility=True)
+    logged_id = fields.Boolean(string='No approval user logged', compute="_active_id")
 
-    @api.depends('logged_jd')
+    @api.depends('user_logged_id','logged_id')
     def _active_id(self):
-        if self.env.user == self.logged_id:
-            self.logged_jd = True
+        if self.env.user == self.user_logged_id:
+            self.logged_id = True
         else:
-            self.logged_jd = False
+            self.logged_id = False
